a9e474f487120755426f6ba90c40b906a3ae2457
Farid Shahy <fkonigy@gmail.com>
2017-10-25 15:50:22 +0330

    rename http folder to net_http1.

--- net_http1/main.go
+++ net_http1/main.go
@@ -8,11 +8,11 @@ import (
 )
 
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()       // parse arguments, you have to call this by yourself
-	fmt.Println(r.Form) // print form information in server side
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
-	fmt.Println(r.Form["url_long"])
+	r.ParseForm()                 // parse arguments, you have to call this by yourself
+	fmt.Println("form: ", r.Form) // print form information in server side
+	fmt.Println("path: ", r.URL.Path)
+	fmt.Println("scheme: ", r.URL.Scheme)
+	fmt.Println("url_long: ", r.Form["url_long"])
 	for k, v := range r.Form {
 		fmt.Println("key:", k)
 		fmt.Println("val:", strings.Join(v, ""))
